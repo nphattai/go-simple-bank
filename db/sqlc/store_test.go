a7cb5b7a398ee94a03e0140fe5909d24de68a7ba
Tai Nguyen <nphattai98@gmail.com>
2023-09-09 19:42:15 +0700

    chore: remove unused code

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -23,10 +23,8 @@ func TestTransferTx(t *testing.T) {
 
 	// run n concurrent transfer transaction
 	for i := 0; i < n; i++ {
-		txName := fmt.Sprintf("tx %v", i+1)
 		go func() {
-			ctx := context.WithValue(context.Background(), txKey, txName)
-			result, err := store.TransferTx(ctx, TransferTxParams{
+			result, err := store.TransferTx(context.Background(), TransferTxParams{
 				FromAccountID: account1.ID,
 				ToAccountID:   account2.ID,
 				Amount:        amount,
