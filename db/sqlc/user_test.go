efc4dd3d52b9abd0f08932344279e9560cb97e44
Tai Nguyen <nphattai98@gmail.com>
2023-09-20 09:29:31 +0700

    feat: update user with nullable parameters in sqlc

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 	"time"
 
@@ -54,4 +55,109 @@ func TestGetUser(t *testing.T) {
 	require.Equal(t, user1.FullName, user2.FullName)
 	require.Equal(t, user1.PasswordChangedAt, user2.PasswordChangedAt)
 	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+}
+
+func TestUpdateUserOnlyFullName(t *testing.T) {
+	user1 := randomUser(t)
+
+	newFullName := util.RandomOwner()
+
+	arg := UpdateUserParams{
+		Username: user1.Username,
+		FullName: sql.NullString{String: newFullName, Valid: true},
+	}
+
+	user2, err := testQueries.UpdateUser(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, arg.Username, user2.Username)
+	require.NotEqual(t, user1.FullName, user2.FullName)
+	require.Equal(t, newFullName, user2.FullName)
+	require.Equal(t, user1.HashedPassword, user2.HashedPassword)
+	require.Equal(t, user1.Email, user2.Email)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+	require.WithinDuration(t, user1.PasswordChangedAt, user2.PasswordChangedAt, time.Second)
+}
+
+func TestUpdateUserOnlyPassword(t *testing.T) {
+	user1 := randomUser(t)
+
+	newPassword := util.RandomString(6)
+	hashedPassword, err := util.HashPassword(newPassword)
+	require.NoError(t, err)
+
+	arg := UpdateUserParams{
+		Username:       user1.Username,
+		HashedPassword: sql.NullString{String: hashedPassword, Valid: true},
+	}
+
+	user2, err := testQueries.UpdateUser(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, user1.Username, user2.Username)
+	require.Equal(t, user1.FullName, user2.FullName)
+	require.NotEqual(t, user1.HashedPassword, user2.HashedPassword)
+	require.Equal(t, hashedPassword, user2.HashedPassword)
+	require.Equal(t, user1.Email, user2.Email)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+	require.WithinDuration(t, user1.PasswordChangedAt, user2.PasswordChangedAt, time.Second)
+}
+
+func TestUpdateUserOnlyEmail(t *testing.T) {
+	user1 := randomUser(t)
+
+	newEmail := util.RandomEmail()
+
+	arg := UpdateUserParams{
+		Username: user1.Username,
+		Email:    sql.NullString{String: newEmail, Valid: true},
+	}
+
+	user2, err := testQueries.UpdateUser(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, arg.Username, user2.Username)
+	require.Equal(t, user1.FullName, user2.FullName)
+	require.Equal(t, user1.HashedPassword, user2.HashedPassword)
+	require.NotEqual(t, user1.Email, user2.Email)
+	require.Equal(t, newEmail, user2.Email)
+
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+	require.WithinDuration(t, user1.PasswordChangedAt, user2.PasswordChangedAt, time.Second)
+}
+
+func TestUpdateUserAllField(t *testing.T) {
+	user1 := randomUser(t)
+
+	newFullName := util.RandomOwner()
+	newEmail := util.RandomEmail()
+	newPassword := util.RandomString(6)
+	hashedPassword, err := util.HashPassword(newPassword)
+
+	require.NoError(t, err)
+	arg := UpdateUserParams{
+		Username:       user1.Username,
+		Email:          sql.NullString{String: newEmail, Valid: true},
+		FullName:       sql.NullString{String: newFullName, Valid: true},
+		HashedPassword: sql.NullString{String: hashedPassword, Valid: true},
+	}
+
+	user2, err := testQueries.UpdateUser(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, user1.Username, user2.Username)
+	require.Equal(t, newFullName, user2.FullName)
+	require.Equal(t, hashedPassword, user2.HashedPassword)
+	require.Equal(t, newEmail, user2.Email)
+
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+	require.WithinDuration(t, user1.PasswordChangedAt, user2.PasswordChangedAt, time.Second)
 }
